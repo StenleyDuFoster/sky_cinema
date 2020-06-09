7d0e5bbf8b7f5562fb4e4a32e84ac72447c80968
Stanley <65017890+StenleyDuFoster@users.noreply.github.com>
2020-06-09 16:10:15 +0300

    Add files via upload

---
+++ ResAdapter/IItemClickListener.cs
@@ -0,0 +1,9 @@
+﻿using Android.Views;
+
+namespace SkyCinema
+{
+    public interface IItemClickListener
+    {
+        void OnClick(View itemView, int i, bool isLongClick);
+    }
+}
---
+++ ResAdapter/ResData.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyCinema
+{
+    public class ResData 
+    {
+        public string imageUrl;
+
+        public string titleText;
+        public string ganreText;//
+        public string mainText;//
+        public string releaseText;
+        public string id;
+    }
+}
